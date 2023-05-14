a82f758d7204e1d2d7a3e9b0e61346631a3f8164
Dragosh Sergey <dragoshs.j@yahoo.com>
2023-05-15 01:02:05 +0300

    Create gradle project with submodules
    Create .gitignore files
    Create root module
    Create account-service submodule
    Create payment-service submodule

---
+++ payment-service/src/main/java/com/dragosh/App.java
@@ -0,0 +1,13 @@
+package com.dragosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
